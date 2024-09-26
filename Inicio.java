761e242ce1efcacfa5b9cb9912e03e2c9297065b
stevenson-caraballo-robles <carolcervante2015@gmail.com>
2024-09-26 13:29:13 -0500

    hice cambios en mi nueva rama

--- Inicio.java
+++ Inicio.java
@@ -1,6 +1,6 @@
 public class Inicio {
 
     public static void main(String[] args) {
-        
+        System.out.println(" esto lo subi desde la rama2 ");
     }
 }
---
+++ mirama.java
@@ -0,0 +1,3 @@
+public class mirama {
+    private String Maria;
+}
